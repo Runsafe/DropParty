0912416ddf408c5f190dfd4bc5f52e72d71fb4ea
Kruithne <kruithne@gmail.com>
2013-05-08 01:49:02 +0100

    Added stupidly long inventory call for science.

--- src/no/runsafe/dropparty/events/InventoryMoveItem.java
+++ src/no/runsafe/dropparty/events/InventoryMoveItem.java
@@ -2,7 +2,6 @@ package no.runsafe.dropparty.events;
 
 import no.runsafe.dropparty.DropHandler;
 import no.runsafe.framework.event.inventory.IInventoryMoveItem;
-import no.runsafe.framework.output.IOutput;
 import no.runsafe.framework.server.block.RunsafeHopper;
 import no.runsafe.framework.server.event.inventory.RunsafeInventoryMoveItemEvent;
 import no.runsafe.framework.server.inventory.IInventoryHolder;
@@ -10,32 +9,27 @@ import no.runsafe.framework.server.item.RunsafeItemStack;
 
 public class InventoryMoveItem implements IInventoryMoveItem
 {
-	public InventoryMoveItem(DropHandler handler, IOutput output)
+	public InventoryMoveItem(DropHandler handler)
 	{
 		this.handler = handler;
-		this.output = output;
 	}
 
 	@Override
 	public void OnInventoryMoveItemEvent(RunsafeInventoryMoveItemEvent event)
 	{
-		this.output.fine("Detected InventoryMoveItemEvent");
 		IInventoryHolder destination = event.getDestination().getHolder();
 		if (destination instanceof RunsafeHopper)
 		{
-			this.output.fine("Detected Hopper move event");
 			RunsafeHopper hopper = (RunsafeHopper) destination;
 			if (hopper.getInventory().getTitle().equalsIgnoreCase("drop_party_consumer"))
 			{
-				this.output.fine("The name of the hopper matched our drop hoppers.");
 				RunsafeItemStack item = event.getItem();
-				event.getSource().remove(item);
-				handler.addItem(item.clone());
+				event.getSource().getHolder().getInventory().remove(item);
+				this.handler.addItem(item.clone());
 				event.setCancelled(true);
 			}
 		}
 	}
 
 	private DropHandler handler;
-	private IOutput output;
 }
