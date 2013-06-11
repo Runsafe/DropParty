80419aae0d0bf6a7685307ce50274ad2060292e0
Morten Nilsen <morten@runsafe.no>
2013-06-11 23:59:18 +0200

    Use public API types

--- src/no/runsafe/dropparty/commands/ClearItems.java
+++ src/no/runsafe/dropparty/commands/ClearItems.java
@@ -1,8 +1,8 @@
 package no.runsafe.dropparty.commands;
 
 import no.runsafe.dropparty.DropHandler;
+import no.runsafe.framework.api.command.ExecutableCommand;
 import no.runsafe.framework.api.command.ICommandExecutor;
-import no.runsafe.framework.internal.command.ExecutableCommand;
 
 import java.util.HashMap;
 
